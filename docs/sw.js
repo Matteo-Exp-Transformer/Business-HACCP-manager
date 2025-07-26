9e2ae9e7ac42de047b0d1c6bd789ac542b044f6f
Matteo-Exp-Transformer <matteo.cavallaro.work@gmail.com>
2025-07-26 03:21:27 +0200

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,20 +1,22 @@
+const BASE_URL = '/Business-HACCP-manager'; // Assicurati che questo corrisponda al nome del tuo repository
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("mini-epackpro-cache").then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/assets/index-CCfqFBtM.css",
-        "/assets/index-1-vZa4Ks.js",
-        "/icons/icon-72x72.png",
-        "/icons/icon-96x96.png",
-        "/icons/icon-128x128.png",
-        "/icons/icon-144x144.png",
-        "/icons/icon-152x152.png",
-        "/icons/icon-192x192.png",
-        "/icons/icon-384x384.png",
-        "/icons/icon-512x512.png"
+        BASE_URL + '/',
+        BASE_URL + '/index.html',
+        BASE_URL + '/manifest.json',
+        BASE_URL + '/asset/index-CCfqFBtM.css',
+        BASE_URL + '/asset/index-1-vZa4Ks.js',
+        BASE_URL + '/icons/icon-72x72.png',
+        BASE_URL + '/icons/icon-96x96.png',
+        BASE_URL + '/icons/icon-128x128.png',
+        BASE_URL + '/icons/icon-144x144.png',
+        BASE_URL + '/icons/icon-152x152.png',
+        BASE_URL + '/icons/icon-192x192.png',
+        BASE_URL + '/icons/icon-384x384.png',
+        BASE_URL + '/icons/icon-512x512.png'
       ]);
     })
   );
